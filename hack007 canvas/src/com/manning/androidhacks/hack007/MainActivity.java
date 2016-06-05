9abe3585dfc52f2a0f5b04ace9f86357a9548638
李林 <ww1095@163.com>
2016-06-05 16:02:57 +0800

    init

---
+++ hack007 canvas/src/com/manning/androidhacks/hack007/MainActivity.java
@@ -0,0 +1,24 @@
+package com.manning.androidhacks.hack007;
+
+import com.manning.androidhacks.hack007.view.DrawView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Display;
+
+public class MainActivity extends Activity {
+
+	private DrawView mDrawView;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Display display 	= getWindowManager().getDefaultDisplay();
+		mDrawView 			= new DrawView(this);
+		mDrawView.height 	= display.getHeight();
+		mDrawView.width 	= display.getWidth();
+
+		setContentView(mDrawView);
+	}
+}
